afabc8ce53aaef331c810e3da23267fcdcaa88d6
hsieh <hsieh@hsiehchingjing>
2017-08-21 16:35:22 +0800

    修改model结构,添加静态字符常定义

---
+++ dataserver/src/main/java/hayaa/dataservice/config/DefineTable.java
@@ -0,0 +1,8 @@
+package hayaa.dataservice.config;
+
+public class DefineTable {
+	public static  String SqlTmpTag_Column="{Column}";
+	public static  String SqlTmpTag_Filter="{Filter}";
+	public static  String SqlTmpTag_GroupBy="{GroupBy}";
+	public static  String SqlTmpTag_OrderBy="{OrderBy}";
+}
---
+++ dataserver/src/main/java/hayaa/dataservice/model/MetaDataResult.java
@@ -0,0 +1,8 @@
+package hayaa.dataservice.model;
+
+public class MetaDataResult {
+	Column[] Columns;
+	FilterItem[] Filters;
+	String[] GroupBys;
+	OrderBy[] OrderBys;
+}
